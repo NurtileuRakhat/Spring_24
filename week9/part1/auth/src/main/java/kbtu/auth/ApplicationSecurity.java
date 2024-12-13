0884426e69f310fb9500135d8da2a4b3e0e5f1dd
Nurtileu <raaaaahaaat@gmail.com>
2024-12-13 13:36:03 +0500

    sis 7 8 9

---
+++ week9/part1/auth/src/main/java/kbtu/auth/ApplicationSecurity.java
@@ -0,0 +1,18 @@
+package kbtu.auth;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeRequests().anyRequest().permitAll();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+}
